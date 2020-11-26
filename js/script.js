25099c5132b827cbfbaf0d8a29b500bc04875eff
kafamem <kafamem@gmail.com>
2020-11-26 10:08:32 +0000

    class counting-down hyphen added, script term display added

--- js/script.js
+++ js/script.js
@@ -1,45 +1,21 @@
-let countingDown = 5;
-let remainingSeconds = 0;
+window.addEventListener('load', init);
 
-//terms to type 
-const term=[
-'Abs',
-'Absolute Strength',
-'Active rest',
-'Aerobic exercise',
-'Aerobics',
-'Agonist muscles',
-'Antagonist muscle',
-'Anterior chain',
-'Back-cycling',
-'Barbell',
-'Bench press',
-'Biceps',
-'Bicep curls',
-'Bleep test',
-'Bodyweight exercises',
-'cardiovascular conditioning',
-'Bulking',
-'Burpees',
-'Cardio',
-'Chest fly',
-'Chest press',
-'Chin up',
-'Circuit training',
-'Core',
-'Crunch',
-'Deadlift',
-'Deltoid muscles',
-'Dumbbell',
-'Endurance training',
-'Exercise ball',
-'Exercise wheel',
-'Fat burn',
-'Hamstrings',
-'Insanity',
-'Kegel exercises',
-'Kettlebell',
-'Lat pull downs',
-'Puss ups',
-]
+let countingDown = 4;
+let gameLoading;
+
+const entryWord = document.querySelector('#entry-word');
+const gymTerms = document.querySelector('#gym-terms');
+const timer = document.querySelector('#counting-down');
+const feedbackMessage = document.querySelector('#feedback');
+
+const terms = ['Abs', 'Absolute Strength', 'Active rest', 'Adduct muscles', 'Aerobic exercise', 'Aerobics', 'Agonist muscles', 'Anaerobic exercise', 'Antagonist muscle', 'Anterior chain', 'Back-cycling', 'Barbell', 'Basal metabolic rate', 'Bench press', 'Biceps', 'Bicep curls', 'Bleep test', 'Body fat percentage', 'Bodyweight exercises', 'Cardiovascular conditioning', 'Bridge', 'Bulking', 'Burpees', 'Calf muscles', 'Cardio', 'Chest fly', 'Chest press', 'Chin up', 'Circuit training', 'Core', 'Crunch', 'Deadlift', 'Deltoid muscles', 'Dumbbell', 'Endurance training', 'Exercise ball', 'Exercise wheel', 'Fat burn', 'Hamstrings', 'Insanity', 'Kegel exercises', 'Kettlebell', 'Lat pull downs'];
+
+function init() {
+    displayTerm(terms);
+}
+
+function displayTerm(terms) {
+    const randomIndex = Math.floor(Math.random() * terms.length);
+    gymTerms.innerHTML = terms[randomIndex];
+}
 
